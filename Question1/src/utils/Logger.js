1b7b35cf6a7611a9ba490a61f643ea592a13d1f4
LakshmiPujakshya <poojapusuluri@gmail.com>
2026-06-05 10:36:21 +0530

    Add files via upload

---
+++ Question1/src/utils/Logger.js
@@ -0,0 +1,22 @@
+export async function Log(stack, level, packageName, message) {
+  const token = "YOUR_TOKEN_HERE";
+
+  const response = await fetch(
+    "http://4.224.186.213/evaluation-service/logs",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        stack,
+        level,
+        package: packageName,
+        message
+      })
+    }
+  );
+
+  return await response.json();
+}
